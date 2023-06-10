2633c35a00fc73749d1e7d0b3b3da4c55f12dbab
hamzabhiinder <hamzabhinder5@gmail.com>
2023-06-10 13:08:25 +0500

    Server

--- index.mjs
+++ index.mjs
@@ -4,30 +4,25 @@ import { } from "./src/connection/db_connection.mjs";
 import authRouter from "./src/routes/auth.mjs";
 import adminRouter from "./src/routes/admin.mjs";
 
-
 import morgan from 'morgan';
 import productRouter from "./src/routes/product.mjs";
+import userRouter from "./src/routes/user.mjs";
 
 
 
 const app = express();
 const port = 3000
 
-app.use(morgan('dev'))
+
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(authRouter)
-app.use(adminRouter)
 app.use(productRouter)
-
+app.use(adminRouter)
+app.use(userRouter)
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on ports ${port} `)
+    console.log(`Example app listening on port ${port} `)
 })
-
-
-
-
-
-
